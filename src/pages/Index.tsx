418c075efcf2a86ed64a88a99e4406beb4ff4bf4
resident0730-commits <resident0730@gmail.com>
2025-09-17 01:51:53 +0500

    Add files via upload

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -70,7 +70,7 @@ const Index = () => {
           <div className="relative">
             <Badge variant="secondary" className="text-lg px-6 py-3 bg-card/50 backdrop-blur-md shadow-gold animate-scale-in">
               <Fuel className="w-5 h-5 mr-2" />
-              Богатство из недр земли
+              ЖОПА ИЗ НЕДР ЗЕМЛИ!!!
             </Badge>
           </div>
           
